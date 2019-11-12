f5a8627580eb0a4d50863d5fe87d493ad5343b6f
Andreas Beasley <andreas@sapioweb.com>
2019-11-11 23:30:01 -0800

    package update and initial app

---
+++ src/app.ts
@@ -0,0 +1,49 @@
+import { json, urlencoded } from 'body-parser';
+import dotenv from 'dotenv';
+import express, { Express, Request, Response, NextFunction } from 'express';
+import { router } from './routes/routes';
+import { connect } from "mongoose";
+
+dotenv.config();
+
+class App {
+    public app: Express;
+
+    constructor() {
+        this.app = express();
+        this.mountDb();
+        this.mountRoutes();
+        this.mountExceptionHandler();
+    };
+
+    private mountRoutes = () => {
+        this.app.use(urlencoded({extended: true}));
+        this.app.use(json());
+
+        this.app.use('/', router);
+    };
+
+    private mountDb = async () => {
+        const {DB_USERNAME, DB_PASSWORD} = process.env;
+
+        if (!DB_PASSWORD && !DB_USERNAME) {
+            return console.log('DB creds not set');
+        }
+
+        await connect(
+            `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@cluster0-d8tan.mongodb.net/test?retryWrites=true&w=majority`,
+            {useNewUrlParser: true, useUnifiedTopology: true}
+        );
+    };
+
+    private mountExceptionHandler = () => {
+        this.app.use((err: Error, _req: Request, res: Response, _next: NextFunction) => {
+            return res.json({
+                success: false,
+                message: err.message
+            });
+        })
+    }
+}
+
+export const app = new App().app;
