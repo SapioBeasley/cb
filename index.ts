8c6dabb35cbbb916a9b5aeb8897d5c939dfe7672
Andreas Beasley <andreas@sapioweb.com>
2019-11-20 05:20:00 -0800

    initial app and index

--- index.ts
+++ index.ts
@@ -1,10 +1,11 @@
 import dotenv from 'dotenv';
-import {app} from './src/app';
+
+import { app } from './src/app';
 
 dotenv.config();
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.info(`http://localhost:${port}`);
+  console.info(`http://localhost:${port}`);
 });
--- src/app.ts
+++ src/app.ts
@@ -1,49 +1,52 @@
 import { json, urlencoded } from 'body-parser';
 import dotenv from 'dotenv';
-import express, { Express, Request, Response, NextFunction } from 'express';
+import e, { Express, NextFunction, Request, Response } from 'express';
+import { connect } from 'mongoose';
+
 import { router } from './routes/routes';
-import { connect } from "mongoose";
 
 dotenv.config();
 
 class App {
-    public app: Express;
+  public app: Express;
 
-    constructor() {
-        this.app = express();
-        this.mountDb();
-        this.mountRoutes();
-        this.mountExceptionHandler();
-    };
+  constructor() {
+    this.app = e();
+    this.mountDb().then(() => console.info('DB connected'));
+    this.mountRoutes();
+    this.mountExceptionHandler();
+  }
 
-    private mountRoutes = () => {
-        this.app.use(urlencoded({extended: true}));
-        this.app.use(json());
+  private readonly mountRoutes = () => {
+    this.app.use(urlencoded({ extended: true }));
+    this.app.use(json());
 
-        this.app.use('/', router);
-    };
+    this.app.use('/', router);
+  };
 
-    private mountDb = async () => {
-        const {DB_USERNAME, DB_PASSWORD, DB_HOST} = process.env;
+  private readonly mountDb = async () => {
+    const { DB_USERNAME, DB_PASSWORD, DB_HOST } = process.env;
 
-        if (!DB_PASSWORD && !DB_USERNAME && !DB_HOST) {
-            return console.log('DB creds not set');
-        }
-
-        await connect(
-            `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}?retryWrites=true&w=majority`,
-            {useNewUrlParser: true, useUnifiedTopology: true}
-        );
-    };
-
-    private mountExceptionHandler = () => {
-        this.app.use((err: Error, _req: Request, res: Response, _next: NextFunction) => {
-            return res.json({
-                success: false,
-                message: err.message
-            });
-        })
+    if (!DB_PASSWORD && !DB_USERNAME && !DB_HOST) {
+      return console.info('DB creds not set');
     }
+
+    await connect(
+      `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}`,
+      { useNewUrlParser: true, useUnifiedTopology: true },
+    );
+  };
+
+  private readonly mountExceptionHandler = () => {
+    this.app.use(
+      (err: Error, _req: Request, res: Response, _next: NextFunction) => {
+        return res.json({
+          success: false,
+          message: err.message,
+        });
+      },
+    );
+  };
 }
 
 export const app = new App().app;
