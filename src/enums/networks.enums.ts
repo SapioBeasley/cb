dce9a152479155c20d4dd13db44125450067d65d
Andreas Beasley <andreas@sapioweb.com>
2019-11-20 21:05:29 -0800

    beautiful enums

---
+++ src/enums/networks.enums.ts
@@ -0,0 +1,5 @@
+export enum Networks {
+  visa = 'visa',
+  mastercard = 'mastercard',
+  unknown = 'unknown'
+}
---
+++ src/enums/platforms.enums.ts
@@ -0,0 +1,4 @@
+export enum Platforms {
+  stripe = 'stripe',
+  shopify = 'shopify',
+}
